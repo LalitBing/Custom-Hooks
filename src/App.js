b4a4bc50ed82d1b9d1bada4c5ca1297ada86a469
Lalit Sharma <lalit9013@gmail.com>
2022-06-04 03:01:24 +0530

    CustomHook Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Example from "./Fetch/fetch-example";
+
+function App() {
+  return (
+    <div className="container">
+      <Example />
+    </div>
+  );
+}
+
+export default App;
